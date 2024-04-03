fc652c513af5ed281083ad0fd0a4de793fd380ad
Madu-f0r0 <duherbert@gmail.com>
2024-04-03 21:42:31 +0100

    Completed the LRUCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,7 +15,8 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """Assigns a new item to the cache dictionary"""
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            mxItems = BaseCaching.MAX_ITEMS
+            if len(self.cache_data) >= mxItems and key not in self.cache_data:
                 discard_key = self.cache_history[-1]
 
                 self.cache_data.pop(discard_key)
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Contains the definition of the class FIFOCache"""
+"""Contains the definition of the class LRUCache"""
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """Implements the FIFO cache replacement technique"""
+class LRUCache(BaseCaching):
+    """Implements the LRU cache replacement technique"""
 
     def __init__(self):
-        """The constructor method for an instance of the class FIFOCache"""
+        """The constructor method for an instance of the class LRUCache"""
         super().__init__()
         self.cache_history = []
 
@@ -16,16 +16,23 @@ class FIFOCache(BaseCaching):
         """Assigns a new item to the cache dictionary"""
         if key is not None and item is not None:
             mxItems = BaseCaching.MAX_ITEMS
+
             if len(self.cache_data) >= mxItems and key not in self.cache_data:
                 discard_key = self.cache_history[0]
-
                 self.cache_data.pop(discard_key)
                 self.cache_history.pop(0)
-
                 print("DISCARD: {}".format(discard_key))
+
+            if key in self.cache_history:
+                self.cache_history.remove(key)
+
             self.cache_data.update({key: item})
             self.cache_history.append(key)
 
     def get(self, key):
         """Retrieves the value in the cache dictionary linked to a key"""
+        if key is not None and key in self.cache_data.keys():
+            self.cache_history.remove(key)
+            self.cache_history.append(key)
+
         return self.cache_data.get(key)
